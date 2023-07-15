5e9eafe19af4ec3b6dd2e48297d6935dfa2cc3e2
andrewlalis <andrewlalisofficial@gmail.com>
2023-07-15 01:21:03 -0400

    Fixed upload file, this time with rsync.

--- upload.sh
+++ upload.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-scp -r * root@andrewlalis.com:/var/www/andrewlalis.com/html
+# Helper script that copies files to the server.
+# You need to have authenticated access to the server via SSH to use this.
+
+rsync -rav -e ssh \
+--exclude .git/ --exclude *.sh --exclude README.md \
+./ \
+root@andrewlalis.com:/var/www/andrewlalis.com/html
